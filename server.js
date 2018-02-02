0300334a93be4de31e4777cb1c9900a07f9910f2
Mark Alaniz <markalaniz000@gmail.com>
2018-02-01 18:41:10 -0600

    basic server setup and module exports complete

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// require API routes
+require("./app/routing/apiRoutes.js")(app);
+// require HTML routes
+require("./app/routing/htmlRoutes.js")(app);
+
+// Server Listening for PORT
+app.listen(PORT, function() {
+  console.log("Server running on PORT: " + PORT);
+});
