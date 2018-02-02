2bf5b7a36c1f91c8effaea755af4909162bfffca
Mark Alaniz <markalaniz000@gmail.com>
2018-02-01 18:07:35 -0600

    initial file tree

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
