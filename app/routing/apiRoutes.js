7dfc4dd711183ed3a2bb7057bd008a5066adb201
Mark Alaniz <markalaniz000@gmail.com>
2018-02-05 16:43:25 -0600

    trying to make a post request to the server with user data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendsArray);
   });
+
+  app.post('/api/friends', function(req, res) {
+    friendsArray.push(req.body);
+  });
 };
