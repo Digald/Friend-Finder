01a64a6385f16e34f39b01e7a1de04e7c02f6b8b
Mark Alaniz <markalaniz000@gmail.com>
2018-02-06 17:11:36 -0600

    created bestMatch function that returns an object of the users match, added a default friend to be matched too for the first user

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,9 @@
-var friendsArray = [];
+var friendsArray = [
+    {
+        name: "Mark",
+        photo: 'https://static.pexels.com/photos/247791/pexels-photo-247791.png',
+        scores: ['3','4','2','1','5','2','4','1','3','2']
+    }
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,20 +6,49 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    var myPost = req.body;
-    console.log(myPost);
-    friendsArray.push(req.body);
+    var currUser = req.body;
+    console.log(currUser);
     // calculate best match based on results
+    // bestMatch(currUser, friendsArray);
     // sends total friends array back to the front-end client so you can return the data
-    res.json(friendsArray);
+    friendsArray.push(req.body);
+    // console.log("from POST: " + JSON.stringify(friendsArray,null,2));
+    res.json(bestMatch(currUser, friendsArray));
   });
 };
 
+function bestMatch(currUser, friendsArray) {
+  // Sum up the total scores from the user currently submitting
+  var mySum = currUser.scores.reduce(add, 0);
+  // Loop through the friendsArray and add a property "scoreTotal" which is the sum of the existing scores of each friend
+  for (var i in friendsArray) {
+    friendsArray[i].scoreTotal = friendsArray[i].scores.reduce(add, 0);
+  }
+  // Make a "current" value for the first friend's total
+  var curr = friendsArray[0].scoreTotal;
+  // Make a default match for the user that will be updated later
+  var match = friendsArray[0];
+  // Calcuate the first different in absolute value and assign that to a variable. This will be our initial difference
+  var diff = Math.abs(mySum - curr);
+  for (var i in friendsArray) {
+    // A newdiff variable will calulate the difference between the current user's submision and the next scoreTotal in the friends array
+    var newdiff = Math.abs(mySum - friendsArray[i].scoreTotal);
+    // if the new difference is closer than the original one defined above, make that new number the original, otherwise throw it away
+    if (newdiff < diff) {
+      diff = newdiff;
+      // set the match equal to that entire object if it has the smallest different
+      var match = friendsArray[i];
+    }
+    
+  }
+  return JSON.stringify(match);
+}
 
-// function bestMatch(myPost, friendsArray) {
-//   var sum = myPost.scores[].reduce(add, 0);
-//   function add(a,b){
-//     return a + b;
-//   }
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
 
+// {
+// 	"name": "Joe",
+// 	"scores": [1,2,3,5,3,4,2,3,3,3]
 // }
